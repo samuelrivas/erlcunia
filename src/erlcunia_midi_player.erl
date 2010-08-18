5d30f1abad50442c907fc35dbfa9e23647a1445a
Samuel Rivas <samuelrivas@gmail.com>
2010-08-18 23:56:46 +0200

    DOC GPLed

--- src/erlcunia_midi_player.erl
+++ src/erlcunia_midi_player.erl
@@ -1,4 +1,22 @@
 %%%-------------------------------------------------------------------
+%%% Copyright 2006, 2007, 2010 Samuel Rivas <samuelrivas@gmail.com>
+%%%
+%%% This file is part of Erlcunia.
+%%%
+%%% Erlcunia is free software: you can redistribute it and/or modify it under
+%%% the terms of the GNU General Public License as published by the Free
+%%% Software Foundation, either version 3 of the License, or (at your option)
+%%% any later version.
+%%%
+%%% Erlcunia is distributed in the hope that it will be useful, but WITHOUT ANY
+%%% WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+%%% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+%%% details.
+%%%
+%%% You should have received a copy of the GNU General Public License along with
+%%% Erlcunia.  If not, see <http://www.gnu.org/licenses/>.
+%%%-------------------------------------------------------------------
+%%%-------------------------------------------------------------------
 %%% File    : player.erl
 %%% Author  : Samuel Rivas <samuel@lambdastream.com>
 %%% Description : Plays midi sounds
